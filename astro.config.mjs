a6396594b12b25ab49fff21c9e117e357f337794
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2025-07-06 15:54:40 -0300

    🤡 feat: create initial website structure with landing page and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import path from 'path';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   vite: {
     resolve: {
       alias: {
